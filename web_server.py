5963faad0564499c8a9b3f82d56746d6bc1aff26
KirillYabl <kirill-yablunovskii@mail.ru>
2023-02-07 16:33:40 +0300

    Limit url number in query

--- web_server.py
+++ web_server.py
@@ -4,7 +4,10 @@ import articles_processor
 
 
 async def process_articles(request: web.Request):
+    max_urls_in_query = 10
     urls = request.rel_url.query.get('urls', '').split(',')
+    if len(urls) > max_urls_in_query:
+        return web.json_response({"error": "too many urls in request, should be 10 or less"}, status=400)
     results = await articles_processor.process_articles_bulk(urls)
     return web.json_response({'urls': results})
 
