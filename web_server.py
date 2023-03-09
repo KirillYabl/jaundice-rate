49a1e4e1c71ad1ece211319c042a48400b2dc0b1
KirillYabl <kirill-yablunovskii@mail.ru>
2023-03-09 18:31:23 +0300

    Make morph singleton

--- web_server.py
+++ web_server.py
@@ -3,7 +3,14 @@ from aiohttp import web
 
 import articles_processor
 
-morph = pymorphy2.MorphAnalyzer()
+
+def get_morph():
+    global morph
+    try:
+        return morph
+    except NameError:
+        morph = pymorphy2.MorphAnalyzer()
+        return morph
 
 
 async def process_articles(request: web.Request):
@@ -11,14 +18,14 @@ async def process_articles(request: web.Request):
     urls = request.rel_url.query.get('urls', '').split(',')
     if len(urls) > max_urls_in_query:
         return web.json_response({"error": "too many urls in request, should be 10 or less"}, status=400)
+    morph = get_morph()
     results = await articles_processor.process_articles_bulk(morph, urls)
     return web.json_response({'urls': results})
 
 
-app = web.Application()
-app.add_routes([
-    web.get('/', process_articles),
-])
-
 if __name__ == '__main__':
+    app = web.Application()
+    app.add_routes([
+        web.get('/', process_articles),
+    ])
     web.run_app(app)
